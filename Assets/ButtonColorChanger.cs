ae90cfcaf2e3209d37e36aecb3bec3259cdbdf66
NicoleLoYuTzu <a4207486@gmail.com>
2024-11-13 01:14:27 +0800

    [add] 進度條123按鈕被選到要將背景反藍並且做出相對應畫面123panel

---
+++ Assets/ButtonColorChanger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonColorChanger : MonoBehaviour
+{
+
+    private Color originalColor; // 用於儲存按鈕的初始顏色
+    public Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 確保按鈕有 Image 組件並儲存初始顏色
+        if (button != null)
+        {
+            originalColor = button.GetComponent<Image>().color;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeColorToBlue()
+    {
+        Color blueColor = Color.blue;
+        button.GetComponent<Image>().color = blueColor;
+    }
+
+    public void ResetColor()
+    {
+        button.GetComponent<Image>().color = originalColor;
+    }
+}
---
+++ Assets/HoverToShow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverToShow : MonoBehaviour
+{
+    public GameObject uiElement; // 要顯示或隱藏的 UI 元素
+
+    private void LogWithName(string message)
+    {
+        Debug.Log($"Nicole: {message}");
+    }
+
+    // 顯示 UI
+    public void Show()
+    {
+        uiElement.SetActive(true); // 將 UI 設置為可見
+        Debug.Log("UI 顯示了！"); // 日誌輸出
+        RectTransform rectTransform = uiElement.GetComponent<RectTransform>();
+
+        // 使用 LogWithName 函数打印位置信息
+        LogWithName($"Preview : MonoBehaviour UI Panel Position: {rectTransform.anchoredPosition}, Scale rectTransform: {rectTransform.localScale}");
+
+    }
+
+    // 隱藏 UI
+    public void Hide()
+    {
+        uiElement.SetActive(false); // 將 UI 設置為隱藏
+        Debug.Log("UI 隱藏了！"); // 日誌輸出
+    }
+}
