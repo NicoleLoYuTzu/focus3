ff0ff43fa13f2938eb5069cd4254082904d016b8
NicoleLoYuTzu <a4207486@gmail.com>
2025-02-24 01:52:03 +0800

    [ADD] 站到毛毛蟲板子上做一件事情

---
+++ Assets/PlayerTextUpdate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using TMPro; // 如果你用 TextMeshPro，請加這行
+
+public class PlayerTextUpdate : MonoBehaviour
+{
+    public TMP_Text infoText; // 使用 TextMeshPro，或改成 `public Text infoText;` 來使用舊版 UI
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // 使用 `other.gameObject.CompareTag` 檢查碰撞物件的 Tag
+        Log($"PlayerTextUpdate other {other}");
+        if (other.gameObject.CompareTag("MainCamera"))
+        {
+            infoText.text = "你站在地面上！";
+            Log("PlayerTextUpdate OnTriggerEnter");
+        }
+    }
+
+    private void Log(string message)
+    {
+        Debug.Log($"PlayerTextUpdate: {message}");
+    }
+}
