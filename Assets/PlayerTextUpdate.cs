241961464f56ff99fef965e6096b3eaaaaaffe91
NicoleLoYuTzu <a4207486@gmail.com>
2025-03-04 03:44:37 +0800

    [add] 偵測是否拿到手套 開啟紅色光束 [next] 改變手套位置 研究轉場 把anchor放到紅色光束中間 畫面進行倒數3秒進到下一場次 先不要用particle 因為後面遊戲都還要研究

--- Assets/PlayerTextUpdate.cs
+++ Assets/PlayerTextUpdate.cs
@@ -4,14 +4,22 @@ using TMPro; // 如果你用 TextMeshPro，請加這行
 public class PlayerTextUpdate : MonoBehaviour
 {
     public TMP_Text infoText; // 使用 TextMeshPro，或改成 `public Text infoText;` 來使用舊版 UI
-
+    public GameObject glove;
+    public GameObject lightBeam;
     private void OnTriggerEnter(Collider other)
     {
         // 使用 `other.gameObject.CompareTag` 檢查碰撞物件的 Tag
         Log($"PlayerTextUpdate other {other}");
         if (other.gameObject.CompareTag("MainCamera"))
         {
-            infoText.text = "你站在地面上！";
+
+            //如果手套有拿到
+            if (glove.active == true)
+            {
+                lightBeam.active = true;
+                infoText.text = "你拿到手套！通往另一世界的大門已開啟!快去探索吧!";
+            }
+            
             Log("PlayerTextUpdate OnTriggerEnter");
         }
     }
