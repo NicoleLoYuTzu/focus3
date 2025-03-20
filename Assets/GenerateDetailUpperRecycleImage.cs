e5bc68ce89409a09d0febdf68be7533be5b8a2a7
NicoleLoYuTzu <a4207486@gmail.com>
2025-03-20 12:45:49 +0800

    [add] 解決了兔子preview找不到毛毛蟲的問題 因為命名到兩個一樣的Canvas名字

--- Assets/GenerateDetailUpperRecycleImage.cs
+++ Assets/GenerateDetailUpperRecycleImage.cs
@@ -104,8 +104,6 @@ public class GenerateDetailUpperRecycleImage : MonoBehaviour
             LogInfo($"Instantiated new button for image: {imagesArray[i].name}");
 
             newButton.name = imagesArray[i].name;
-            PrefabHoverHandler hoverHandler = newButton.AddComponent<PrefabHoverHandler>();
-            LogInfo($"Added PrefabHoverHandler to: {newButton.name}");
 
 
             if (imagesArray[i].name == "purpleGhost") {
