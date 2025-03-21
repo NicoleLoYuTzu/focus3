92454ed9a7468ab58f1821a57384f16f21aa5f34
NicoleLoYuTzu <a4207486@gmail.com>
2025-03-21 17:02:41 +0800

    [add] 幫毛毛蟲用介面ok next 會一直跑到毛毛蟲的地下 還有城牆 做building 換地圖的那個anchor要看一下

---
+++ Assets/CaterpillarGameManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using TMPro;
+
+public class CaterpillarGameManager : MonoBehaviour
+{
+    public TMP_Text infoText;
+    public GameObject glove;
+    public GameObject lightBeam;
+    public GameObject lightBeamAnchor;
+    public UnityEngine.XR.Interaction.Toolkit.Interactables.XRBaseInteractable hintButton; // 提示1按鈕
+    public UnityEngine.XR.Interaction.Toolkit.Interactables.XRBaseInteractable passButton; // 提示2按鈕
+
+    private UnityEngine.XR.Interaction.Toolkit.Interactors.XRBaseInteractor hoveringInteractor;
+
+    private void Start()
+    {
+        lightBeam.SetActive(false);
+        lightBeamAnchor.SetActive(false);
+
+        // 監聽 Hover 進出事件
+        hintButton.hoverEntered.AddListener(OnHoverEnter);
+        hintButton.hoverExited.AddListener(OnHoverExit);
+
+        passButton.hoverEntered.AddListener(OnHoverEnter);
+        passButton.hoverExited.AddListener(OnHoverExit);
+    }
+
+    private void Update()
+    {
+        if (hoveringInteractor != null)
+        {
+            // 取得左右手控制器
+            InputDevice leftHandDevice = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+            InputDevice rightHandDevice = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+
+            bool isPressedLeft = false, isPressedRight = false;
+
+            // 偵測 Trigger 按鈕
+            InputHelpers.IsPressed(leftHandDevice, InputHelpers.Button.Trigger, out isPressedLeft);
+            InputHelpers.IsPressed(rightHandDevice, InputHelpers.Button.Trigger, out isPressedRight);
+
+            if (isPressedLeft || isPressedRight)
+            {
+                Debug.Log("CaterpillarGameManager Trigger button pressed!");  // 🔹 記錄按鈕是否被按到
+
+                // 判斷 Hover 的物件來顯示不同的提示
+                if (hoveringInteractor.interactablesHovered.Contains(hintButton))
+                {
+                    Debug.Log("CaterpillarGameManager Hint button pressed!");  // 🔹 檢查是否偵測到 hintButton
+                    infoText.text = "「哦，是你啊，來這裡做什麼？你應該知道我不會隨便幫忙的吧。除非……你找到那隻笨兔子的手套，那可是打開秘密大門的關鍵。我可沒耐心等太久，去吧，等你拿到手套再來找我。」";
+                }
+                else if (hoveringInteractor.interactablesHovered.Contains(passButton))
+                {
+                    Debug.Log("CaterpillarGameManager Pass button pressed!");  // 🔹 檢查是否偵測到 passButton
+
+                    if (glove.activeSelf == true)
+                    {
+                        Debug.Log("CaterpillarGameManager Glove is active! Unlocking door...");  // 🔹 確保手套狀態正確
+                        lightBeam.SetActive(true);
+                        lightBeamAnchor.SetActive(true);
+                        infoText.text = "你拿到手套！通往另一世界的大門已開啟!快去探索吧!";
+                    }
+                    else
+                    {
+                        Debug.Log("CaterpillarGameManager Glove is NOT active!");  // 🔹 提示手套沒有被取得
+                        infoText.text = "你沒有給我手套, 我是不可能開啟另一世界的大門的!~~";
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnHoverEnter(HoverEnterEventArgs args)
+    {
+        hoveringInteractor = args.interactorObject as UnityEngine.XR.Interaction.Toolkit.Interactors.XRBaseInteractor;
+        Debug.Log("CaterpillarGameManager Hover enter: " + args.interactableObject);  // 🔹 記錄 Hover 進入的物件
+    }
+
+    private void OnHoverExit(HoverExitEventArgs args)
+    {
+        Debug.Log("CaterpillarGameManager Hover exit: " + args.interactableObject);  // 🔹 記錄 Hover 離開的物件
+        hoveringInteractor = null;
+    }
+}
--- Assets/PlayerTextUpdate.cs
+++ Assets/PlayerTextUpdate.cs
@@ -3,10 +3,12 @@ using TMPro; // 如果你用 TextMeshPro，請加這行
 
 public class PlayerTextUpdate : MonoBehaviour
 {
-    public TMP_Text infoText; // 使用 TextMeshPro，或改成 `public Text infoText;` 來使用舊版 UI
-    public GameObject glove;
-    public GameObject lightBeam;
-    public GameObject lightBeamAnchor;
+    //public TMP_Text infoText; // 使用 TextMeshPro，或改成 `public Text infoText;` 來使用舊版 UI
+    public GameObject hintCanvas;
+
+    void Start(){
+        hintCanvas.SetActive(false);
+    }
     private void OnTriggerEnter(Collider other)
     {
         // 使用 `other.gameObject.CompareTag` 檢查碰撞物件的 Tag
@@ -14,17 +16,32 @@ public class PlayerTextUpdate : MonoBehaviour
         if (other.gameObject.CompareTag("MainCamera"))
         {
 
-            //如果手套有拿到
-            if (glove.active == true)
-            {
-                lightBeam.active = true;
-                lightBeamAnchor.active = true;
-                infoText.text = "你拿到手套！通往另一世界的大門已開啟!快去探索吧!";
-            }
-            
+            ////如果手套有拿到
+            //if (glove.active == true)
+            //{
+            //    lightBeam.active = true;
+            //    lightBeamAnchor.active = true;
+            //    infoText.text = "你拿到手套！通往另一世界的大門已開啟!快去探索吧!";
+            //}
+            hintCanvas.SetActive(true);
+
+
             Log("PlayerTextUpdate OnTriggerEnter");
         }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // 當 MainCamera 離開時，隱藏提示 UI
+        if (other.gameObject.CompareTag("MainCamera"))
+        {
+            Debug.Log("MainCamera 離開觸發區，隱藏提示 UI");
+
+            hintCanvas.SetActive(false);
+        }
+    }
+
+
 
     private void Log(string message)
     {
